386a76e984c89ec02653477e548b80f879c44931
Aziz <abdouazizk50@gmail.com>
2022-07-28 02:07:37 +0000

    creation du crud

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const app = require('./app');
+
+const normalizerPort  = val => {
+    const port = parseInt(val, 10);
+    
+    if(isNaN(port)){
+        return val;
+    }
+    if(port >= 0){
+        return port;
+    }
+    return false;
+
+};
+
+const port = normalizerPort(process.env.PORT || 3000);
+app.set('port', port);
+
+
+const errorHandler = error => {
+    if(error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const address =  server.address();
+    const bind = typeof address === 'string' ? 'pipe'+ address: 'port'+port;
+    
+    switch(error.code){
+        case 'EACCES':
+            console.log(bind + 'requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.log(bind + 'is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address =  server.address();
+    const bind = typeof address === 'string' ? 'pipe'+ address: 'port'+port;
+    console.log('Listening on ' + port);
+});
+
+
+server.listen(port);
+
+
+
